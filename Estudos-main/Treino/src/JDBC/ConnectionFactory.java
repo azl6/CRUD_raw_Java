dadd9cd131f329a81ec43f5cc6dc667295051396
asord66 <80921933+asord66@users.noreply.github.com>
2021-08-10 16:30:35 -0300

    Add files via upload

---
+++ Estudos-main/Treino/src/JDBC/ConnectionFactory.java
@@ -0,0 +1,80 @@
+package JDBC;
+
+import JDBC.Classes.Comprador;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConnectionFactory {
+
+    public static Connection getConnection() {
+        String url = "jdbc:mysql://localhost:3306/agencia";
+        String user = "root";
+        String password = "jeisonsuper1";
+
+        Connection con = null;
+        try {
+            con = DriverManager.getConnection(url, user, password);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
+
+    public static void closeConnection(Connection con) {
+        try {
+            con.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection con, Statement stmt) {
+        closeConnection(con);
+        try {
+            stmt.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void closeConnection(Connection con, Statement stmt, ResultSet rs) {
+        closeConnection(con,stmt);
+        try {
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void add(Connection con, String command) {
+        try {
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(command);
+            closeConnection(con, stmt);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void delete(Connection con, String command) {
+        try {
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(command);
+            closeConnection(con, stmt);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void update(Connection con, String command) {
+        try {
+            Statement stmt = con.createStatement();
+            stmt.executeUpdate(command);
+            closeConnection(con, stmt);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
